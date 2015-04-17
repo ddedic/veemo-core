e1555b4509b87a49e509423e293ca340435ca01a
ddedic@kodio <dedic.d@gmail.com>
2015-04-17 15:51:25 +0000

    koding, middlewares, frontend/backend

---
+++ src/Http/routes.php
@@ -0,0 +1,59 @@
+<?php
+
+
+/*
+|--------------------------------------------------------------------------
+| Default Frontend Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the frontend routes for the module.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::group(['namespace' => 'Veemo\Core\Http\Controllers\Frontend'], function () {
+
+    Route::get('/', ['uses' => 'FrontendController@getHomepage', 'as' => 'frontend.homepage']);
+
+});
+
+
+/*
+|--------------------------------------------------------------------------
+| Default Backend Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the backend routes for the module.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::group(config('veemo.core.backendRouteSettings'), function () {
+
+
+    Route::get('/', ['as' => 'backend', 'middleware' => 'auth.backend'], function() {
+        return redirect()->route('backend.dashboard');
+    });
+
+
+});
+
+
+/*
+|--------------------------------------------------------------------------
+| Default API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the API routes for the module.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::group(['prefix' => 'api'], function () {
+
+    // API routes
+
+});
--- src/Publish/Config/core.php
+++ src/Publish/Config/core.php
@@ -45,6 +45,26 @@ return array(
     */
 
     'backendPrefix' => 'backend',
+    
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Backend route settings
+    |--------------------------------------------------------------------------
+    |
+    |
+    | The prefix that will be used for the administration. Usually admin or backend
+    |
+    |
+    |
+    */
+
+    'backendRouteSettings' => [
+        'prefix'        => 'backend',
+        'domain'        => '',
+        'namespace'     => 'Backend',
+        'middleware'    => 'auth.backend'
+    ]
 
 
 );
